71223faccbd2c8e4bb1444c65448536ac0735386
irham37 <irham.muhammad37@gmail.com>
2014-11-28 14:55:51 +0700

    Update pesanan.java

--- app/models/pesanan.java
+++ app/models/pesanan.java
@@ -9,14 +9,23 @@ import javax.persistence.OneToOne;
 import play.db.jpa.Model;
 @Entity
 public class pesanan extends Model{
+	public String namaPesanan;
 	public String status;
 	@ManyToOne
 	public nomeja nomeja ;
+	
 	public Date tgl ;
 	public int jumlah ;
 	public Double total ;
-	@ManyToOne
-	public menu menu ;
+	
+	@OneToMany(cascade=CascadeType.ALL, mappedBy="mnu1")
+	public List<menu> idMenu1;
+	
 	@OneToMany 
 	public orang orang;
+	
+	public String toString(){
+		return this.namaPesanan;
+	}
+
 }
